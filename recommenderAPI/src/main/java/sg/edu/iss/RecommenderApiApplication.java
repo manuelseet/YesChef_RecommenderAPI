cfb30bec24973efd6634d4a76011f006d957900c
manuelseet <manuelseet@gmail.com>
2022-01-27 12:13:18 +0800

    First commit

---
+++ recommenderAPI/src/main/java/sg/edu/iss/RecommenderApiApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.iss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommenderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommenderApiApplication.class, args);
+	}
+
+}
