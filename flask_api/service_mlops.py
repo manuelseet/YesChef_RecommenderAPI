0023a68f0750fac3993cbdcdbef529e345f03cb5
manuelseet <manuelseet@gmail.com>
2022-02-10 07:10:03 +0800

    Modified MLOps to run only when updates needed

--- flask_api/service_mlops.py
+++ flask_api/service_mlops.py
@@ -49,70 +49,73 @@ def updateFeatureVector(dbname):
     print("#################> entering feature vector method##############")
 
     #=========== PREPARE DATA ===========
-    recipe_list = get_all_recipes(dbname)
-    feature_collection = get_feature_vector(dbname)
+    feature_vectors = get_feature_vector(dbname)
+    all_recipeIDs = [str(x["_id"]) for x in get_all_recipes(dbname)]
 
-    all_recipeIDs = []
+    if all_recipeIDs == list(feature_vectors.index):
+        print("MongoDB Feature Vector is Updated. No Need for MLOps now")
+        print("#################> exiting feature vector method##############")
+    else:
+        #=========== GET INGREDIENT VOCAB ===========
+        recipe_list = get_all_recipes(dbname)
+        ingredient_vocab = []
+        for recipe in recipe_list:
+            if "ingredients" in recipe:
+                ingredient_list = recipe["ingredients"]
+                for ing in ingredient_list:
+                    if "ingredient" in ing:
+                        ing_name = ing["ingredient"]
+                        if ing_name != None:
+                            ingredient_vocab.append(ing_name.lower())
+        ingredient_vocab = list(set(ingredient_vocab))
+        
+        #===========. ENGINEERING FEATURE VOCAB ===========
+        NER_engine = spacy.load("en_core_web_sm")
+        
+        parsed_ingredient_vocab = []
+        for ing in ingredient_vocab: 
+            ing_parsed = my_ingredient_parser(ing, NER_engine, False)
+            parsed_ingredient_vocab.append(ing_parsed)
 
-    #=========== GET INGREDIENT VOCAB ===========
-    ingredient_vocab = []
-    for recipe in recipe_list:
-        if "ingredients" in recipe:
-            ingredient_list = recipe["ingredients"]
-            for ing in ingredient_list:
-                if "ingredient" in ing:
-                    ing_name = ing["ingredient"]
-                    if ing_name != None:
-                        ingredient_vocab.append(ing_name.lower())
-    ingredient_vocab = list(set(ingredient_vocab))
-    
-    #===========. ENGINEERING FEATURE VOCAB ===========
-    NER_engine = spacy.load("en_core_web_sm")
-    
-    parsed_ingredient_vocab = []
-    for ing in ingredient_vocab: 
-        ing_parsed = my_ingredient_parser(ing, NER_engine, False)
-        parsed_ingredient_vocab.append(ing_parsed)
+        parsed_ingredient_vocab = list(set(parsed_ingredient_vocab))
+        print(len(parsed_ingredient_vocab))
+        parsed_ingredient_vocab.insert(0, "recipeID")
+        print("-------->finished parsing method ", parsed_ingredient_vocab[:10])
 
-    parsed_ingredient_vocab = list(set(parsed_ingredient_vocab))
-    print(len(parsed_ingredient_vocab))
-    parsed_ingredient_vocab.insert(0, "recipeID")
-    print("-------->finished parsing method ", parsed_ingredient_vocab[:10])
+        
 
-    
+        #=========== FEATURE VECTORIZATION ===========
+        allBOWs = []
+        recipe_list = get_all_recipes(dbname)
+        for recipe in recipe_list:
+            recipe_ing = []
+            if "ingredients" in recipe:
+                ingredient_list = recipe["ingredients"]
+                for ing in ingredient_list:
+                    if "ingredient" in ing:
+                        ing_name = ing["ingredient"]
+                        if ing_name != None:
+                            ing_name = ing_name.lower()
+                            ing_parsed = my_ingredient_parser(ing_name, NER_engine, False)
+                            if ing_parsed in parsed_ingredient_vocab:
+                                recipe_ing.append(ing_parsed)
+            bow0 = calculateBOW(parsed_ingredient_vocab,recipe_ing)
+            recipeID = str(recipe["_id"])
+            bow0["recipeID"] = recipeID
+            allBOWs.append(bow0)
 
-    #=========== FEATURE VECTORIZATION ===========
-    allBOWs = []
-    recipe_list = get_all_recipes(dbname)
-    for recipe in recipe_list:
-        recipe_ing = []
-        if "ingredients" in recipe:
-            ingredient_list = recipe["ingredients"]
-            for ing in ingredient_list:
-                if "ingredient" in ing:
-                    ing_name = ing["ingredient"]
-                    if ing_name != None:
-                        ing_name = ing_name.lower()
-                        ing_parsed = my_ingredient_parser(ing_name, NER_engine, False)
-                        if ing_parsed in parsed_ingredient_vocab:
-                            recipe_ing.append(ing_parsed)
-        bow0 = calculateBOW(parsed_ingredient_vocab,recipe_ing)
-        recipeID = str(recipe["_id"])
-        bow0["recipeID"] = recipeID
-        allBOWs.append(bow0)
+        #=========== PUSHING TO MONGO DB ===========
+        df_bow = pd.DataFrame(allBOWs)
+        df_bow.set_index("recipeID", inplace = True)
+        df_bow_new = df_bow.drop([""], axis = 1)
 
-    #=========== PUSHING TO MONGO DB ===========
-    df_bow = pd.DataFrame(allBOWs)
-    df_bow.set_index("recipeID", inplace = True)
-    df_bow_new = df_bow.drop([""], axis = 1)
+        df_bow_dict = df_bow.to_dict('index')
+        f_vec_collection = "feature_vector"
+        feature_collection_mongo = dbname[f_vec_collection]
+        feature_collection_mongo.delete_many({})
+        feature_collection_mongo.insert_one(df_bow_dict)
 
-    df_bow_dict = df_bow.to_dict('index')
-    f_vec_collection = "feature_vector"
-    feature_collection = dbname[f_vec_collection]
-    feature_collection.delete_many({})
-    feature_collection.insert_one(df_bow_dict)
-
-    #=========== PRINT TO EXCEL ===========  
-    countRecipes = df_bow_new.sum()
-    countRecipes.to_excel("feature_vec.xlsx")
+        #=========== PRINT TO EXCEL ===========  
+        countRecipes = df_bow_new.sum()
+        countRecipes.to_excel("feature_vec.xlsx")
     
