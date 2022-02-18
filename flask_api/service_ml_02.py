98e9d893aa7ee0f311fd4e4f2c08645ae69f0eea
manuelseet <manuelseet@gmail.com>
2022-02-18 13:58:32 +0800

    Clean up junk codes

--- flask_api/service_ml_02.py
+++ flask_api/service_ml_02.py
@@ -16,25 +16,3 @@ def obtain_more_like_this_reco(recipeId, max, dbname):
     recommendations = [
         x[0] for x in this_recipe_sm_np[this_recipe_sm_np['jaccard'] < 1][-max:]]
     return recommendations
-
-
-"""
-def obtain_more_like_this_reco(recipeId, max, dbname):
-
-    # 1. GET FEATURE VECTOR
-    feature_vectors = get_feature_vector(dbname)
-
-    # 2. GET QUERY RECIPE ID
-    query_recipeID = recipeId
-
-    # 3. PREPARE SEARCH CORPUS & QUERY DOC
-    query_feature_vector = pd.DataFrame(feature_vectors.loc[query_recipeID]).T
-    test_feature_vector = feature_vectors.drop([query_recipeID], axis=0)
-
-    # 4. CALCULATE SIMILARITY SCORES (based on OHE feature vector)
-    js_sorted = get_recipes_sortby_jaccard(
-        test_feature_vector, query_feature_vector)
-    recommendations = js_sorted.index.tolist()[0:max]
-
-    return recommendations
-"""
